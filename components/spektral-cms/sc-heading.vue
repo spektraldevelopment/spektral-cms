5e7b7ba70b6578b2f3e02841b49a80773f814bd2
Dave Boyle <spektraldevelopment@gmail.com>
2019-07-19 08:48:11 -0400

    [develop] - Fleshed out editing flow.

--- components/spektral-cms/sc-heading.vue
+++ components/spektral-cms/sc-heading.vue
@@ -1,26 +1,58 @@
 <template>
     <div>
-        <h1 v-show="editMode === false">{{mutableText}}</h1>
-        <input v-show="editMode" v-model="mutableText" @input="onInputUpdate"/>
+        <h1 v-if="type === 'h1'" v-show="inEditMode === false">{{mutableText}}</h1>
+        <h2 v-if="type === 'h2'" v-show="inEditMode === false">{{mutableText}}</h2>
+        <h3 v-if="type === 'h3'" v-show="inEditMode === false">{{mutableText}}</h3>
+        <h4 v-if="type === 'h4'" v-show="inEditMode === false">{{mutableText}}</h4>
+        <h5 v-if="type === 'h5'" v-show="inEditMode === false">{{mutableText}}</h5>
+        <h6 v-if="type === 'h6'" v-show="inEditMode === false">{{mutableText}}</h6>
+        <input v-show="inEditMode" v-model="mutableText" @input="onInputUpdate"/>
+        <div v-show="editMode">
+            <button v-show="inEditMode === false" @click="onEdit">Edit</button>
+            <button v-show="inEditMode" @click="onSave">Save</button>
+            <button v-show="inEditMode" @click="onCancel">Cancel</button>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['innerText', 'editMode'],
+    props: {
+        innerText: {
+            type: String
+        },
+        editMode: {
+            type: Boolean,
+            default: false
+        },
+        type: {
+            type: String,
+            default: 'h1'
+        }
+    },
     data() {
         return {
-            mutableText: this.innerText
+            mutableText: this.innerText,
+            inEditMode: false
         }
     },
     methods: {
         onInputUpdate(evt) {
             this.mutableText = evt.target.value;
-        }
-    },
-    watch: {
-        editMode(newVal, oldVal) {
-            console.log("Edit Mode: newVal: ", newVal, " oldVal: ", oldVal);
+        },
+        onEdit() {
+            if(this.inEditMode) {
+                this.inEditMode = false;
+            } else {
+                this.inEditMode = true;
+            }
+        },
+        onSave() {
+            this.inEditMode = false;
+        },
+        onCancel() {
+            this.inEditMode = false;
+            this.mutableText = this.innerText;
         }
     }
 }
